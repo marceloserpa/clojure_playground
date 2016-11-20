bae4d7d59135e0e1999053fda1906aa252c1b575
marceloserpa <marcelo.serpa@ilegra.com>
2016-11-20 13:45:39 -0200

    Connecting with mongodb to insert and get all

--- book-service-rest/project.clj
+++ book-service-rest/project.clj
@@ -1,8 +1,9 @@
 (defproject clojure-rest "0.1.0-SNAPSHOT"
   :description "REST service for documents"
   :url "http://blog.interlinked.org"
-  :dependencies [[org.clojure/clojure "1.4.0"]
-                 [compojure "1.1.1"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [compojure/compojure "1.5.1"]
+                 [com.novemberain/monger "3.1.0"]
                  [ring/ring-json "0.1.2"]
                  [ring/ring-jetty-adapter "0.3.7"]
                  [cheshire "4.0.3"]]
--- book-service-rest/src/clojure-rest/core.clj
+++ book-service-rest/src/clojure-rest/core.clj
@@ -2,10 +2,32 @@
   (:use compojure.core)
   (:use ring.adapter.jetty)
   (:require [compojure.handler :as handler]
+            [monger.core :as mg]
+            [monger.collection :as mc]
+            [monger.conversion :refer [from-db-object]]
+
+            [ring.middleware.json :as ring-json]
+
+            [compojure.core :refer :all]
             [compojure.route :as route]))
 
 (defroutes routes-app
-           (GET "/" [] "Hello World")
-           (route/not-found "Not Found"))
+   (GET "/books/" []
+     (let [conn (mg/connect)
+           db   (mg/get-db conn "test")]
+       (mc/find-maps db :books {})))
 
-(run-jetty routes-app {:port 9001})
+   (POST "/books/" {params :params}
+      (let [conn (mg/connect)
+            db   (mg/get-db conn "test")
+            author "ooooo"
+            title "hhhhhhh"]
+        (mc/insert db :books params))
+      "OK")
+
+   (route/not-found "Not Found"))
+
+(run-jetty
+  (ring-json/wrap-json-response
+    (ring-json/wrap-json-params routes-app)) {:port 9001})
+
