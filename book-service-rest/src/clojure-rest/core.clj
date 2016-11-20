83b07a48599faba4d1ce538ac25a55f582d0cd1d
marceloserpa <marcelo.serpa@ilegra.com>
2016-11-20 15:21:57 -0200

    added more operation to api

--- book-service-rest/src/clojure-rest/core.clj
+++ book-service-rest/src/clojure-rest/core.clj
@@ -5,26 +5,34 @@
             [monger.core :as mg]
             [monger.collection :as mc]
             [monger.conversion :refer [from-db-object]]
-
             [ring.middleware.json :as ring-json]
-
             [compojure.core :refer :all]
-            [compojure.route :as route]))
+            [compojure.route :as route])
+  (:import (org.bson.types ObjectId)))
 
 (defroutes routes-app
    (GET "/books/" []
      (let [conn (mg/connect)
            db   (mg/get-db conn "test")]
        (mc/find-maps db :books {})))
-
    (POST "/books/" {params :params}
       (let [conn (mg/connect)
             db   (mg/get-db conn "test")
-            author "ooooo"
-            title "hhhhhhh"]
-        (mc/insert db :books params))
+            author (get-in params ["author"])
+            title (get-in params ["title"])]
+        (mc/insert db :books {:author author :title title}))
       "OK")
-
+   (GET "/books/:id" [id]
+     (let [conn (mg/connect)
+           db   (mg/get-db conn "test")]
+       (mc/find-maps db :books {:_id (ObjectId. id)})))
+   (context "/books/:oid" [oid]
+     (PUT "/" {params :params}
+       (let [conn (mg/connect)
+             db   (mg/get-db conn "test")
+             author (get-in params ["author"])
+             title (get-in params ["title"])]
+         (mc/update db "books" {:_id (ObjectId. oid)} {:author author :title title}) "ok")))
    (route/not-found "Not Found"))
 
 (run-jetty
